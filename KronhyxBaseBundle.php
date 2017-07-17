7c61f26b0ea765977bb925d7824c22b834848e3a
kronhyx <randy@ecc.cu>
2017-07-17 09:15:47 -0400

    Add phpDoc

--- KronhyxBaseBundle.php
+++ KronhyxBaseBundle.php
@@ -4,6 +4,10 @@ namespace Kronhyx\BaseBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class KronhyxBaseBundle
+ * @package Kronhyx\BaseBundle
+ */
 class KronhyxBaseBundle extends Bundle
 {
 }
